d9e16d70188e120e10b6449fe91f89e7a056cffd
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-01-14 20:45:03 +0200

    feat: enhance App layout and update CSS for improved styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import WindowControls from "./components/window_controller";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <main className="container ">
-      <WindowControls title="kiasin"/>
-      <Outlet/>
+    <main className="bg-dark-900 min-h-screen ">
+      <WindowControls title="kiasin" />
+      <div className="app_container">
+        <Outlet />
+      </div>
+      <Navbar />
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,7 @@ import { router } from "./Router";
 import { Provider } from "react-redux";
 import "./index.css";
 import { store } from "./contexts/store";
-import { DraggableHeader } from "./events/drag_handlers";
 
-
-
-
-// Initialize when DOM is ready
-document.addEventListener('DOMContentLoaded', () => {
-  new DraggableHeader('window_header');
-});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
