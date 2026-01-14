5da31448eb17cf460cd0884310c2aa60af45fb2b
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-01-14 20:46:51 +0200

    feat: add Area component with layout and styling

--- src/pages/area/index.tsx
+++ src/pages/area/index.tsx
@@ -1,6 +1,19 @@
+import Header from "./components/header"
+import CardContainer from "./containers/card_container"
+import MainGrid from "./containers/main_gird"
+import "./index.css"
 function Area() {
   return (
-    <div>Area</div>
+    <div className="area_container">
+      <Header/>
+      <MainGrid>
+        <CardContainer columns={2} rows={1}>hello</CardContainer>
+        <CardContainer columns={2} rows={1}>dude</CardContainer>
+        <CardContainer columns={2} rows={1}></CardContainer>
+        <CardContainer columns={2} rows={1}></CardContainer>
+        <CardContainer columns={2} rows={1}></CardContainer>
+      </MainGrid>
+    </div>
   )
 }
 export default Area
