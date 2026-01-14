23022c798a4f8d66f200c894804a168f8c94aaa5
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-01-14 20:46:30 +0200

    feat: add area context with createAsyncThunk and initial state

---
+++ src/contexts/area/functions/create.ts
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { Area } from '..'
+import { Channel, invoke } from '@tauri-apps/api/core'
+
+let create_area = createAsyncThunk('area', async () => {
+  try {
+    let channel = new Channel<Area[]>((state)=>{
+        return state[0]
+    })
+    invoke('areas_control', { command: 'create',channel })
+  } catch (err) {
+    console.log('there is a problem creating an area: ', err)
+  }
+})
---
+++ src/contexts/area/index.ts
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export interface Area {
+  items: any[]
+}
+let init:Area = {
+  items: []
+}
+
+let areas = createSlice({
+  name: 'area',
+  initialState: init,
+  reducers: {
+  }
+})
