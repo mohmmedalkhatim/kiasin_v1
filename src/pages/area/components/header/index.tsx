732de89b4e6564d184d5069adcc9d29e9573be4f
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-01-16 09:51:30 +0200

    feat: add Header component and update Area layout with Content integration

---
+++ src/pages/area/components/header/index.tsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <div className="flex items-center w-full h-18 border-b border-border-main px-8">
+        Header
+    </div>
+  )
+}
+export default Header
--- src/pages/area/index.tsx
+++ src/pages/area/index.tsx
@@ -1,17 +1,16 @@
 import Header from "./components/header"
 import CardContainer from "./containers/card_container"
+import Content from "./containers/card_container/content"
 import MainGrid from "./containers/main_gird"
 import "./index.css"
 function Area() {
   return (
     <div className="area_container">
-      <Header/>
+      <Header />
       <MainGrid>
-        <CardContainer columns={2} rows={1}>hello</CardContainer>
-        <CardContainer columns={2} rows={1}>dude</CardContainer>
-        <CardContainer columns={2} rows={1}></CardContainer>
-        <CardContainer columns={2} rows={1}></CardContainer>
-        <CardContainer columns={2} rows={1}></CardContainer>
+        <CardContainer columns={2} rows={1}>
+          <Content card_type="areas" />
+        </CardContainer>
       </MainGrid>
     </div>
   )
