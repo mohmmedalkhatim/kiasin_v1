a84f8296ff5f1579606fb4f378dca94ab5802db1
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-01-17 11:51:46 +0200

    feat: integrate gsap and lenis for enhanced scrolling effects and database loading

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,36 @@ import { router } from "./Router";
 import { Provider } from "react-redux";
 import "./index.css";
 import { store } from "./contexts/store";
+import gsap from 'gsap';
+import Lenis from 'lenis';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import { load } from '@tauri-apps/plugin-store';
+import Database from '@tauri-apps/plugin-sql';
+
+
+
+export let DB: Database;
+
+
+(async () => {
+  DB =  await Database.load(`sqlite:Database/test.db`);
+})()
+
+const lenis = new Lenis();
+
+export const storage = load("event.json");
+
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 3000);
+});
+
+
+gsap.ticker.lagSmoothing(0);
+
+gsap.registerPlugin(ScrollTrigger);
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
