269d3f663c013d661c7a5863b95959191e841909
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-01-22 11:07:55 +0200

    feat: refactor card creation logic to use a dedicated state creation function

--- src/contexts/area/functions/card/create.ts
+++ src/contexts/area/functions/card/create.ts
@@ -1,16 +1,24 @@
-import {
-  createAsyncThunk,
-} from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 import { retrieve } from '../retrieve'
 import { RootState } from '../../../store'
+import { Card } from '../..'
 
+let create_card_state = (id: number, type: string): Card => ({
+  id,
+  type,
+  store: {},
+  size: {
+    columns: 1,
+    rows: 1
+  }
+})
 export let create_card = createAsyncThunk(
   'area/create_card',
   (payload: { type: string; area_id: number }, api) => {
     let { area_id, type } = payload
     api.dispatch(retrieve(area_id))
     let { area } = api.getState() as RootState
-    let { structure } = area.active
-    structure.cards.push({ id: structure.cards.length, type, store: {} })
+    let { structure } = area.active.area
+    structure.cards.push(create_card_state(structure.cards.length, type))
   }
 )
