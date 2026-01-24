66513e796f4533f60a2876cc3693ff5dabae7249
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-01-24 13:21:42 +0200

    fix: removing the loading changing from the context component

--- src/contexts/area/functions/update.ts
+++ src/contexts/area/functions/update.ts
@@ -2,14 +2,14 @@ import { ActionReducerMapBuilder, createAsyncThunk } from '@reduxjs/toolkit'
 import { Area, areas_storage } from '..'
 import { Channel, invoke } from '@tauri-apps/api/core'
 
-export let update = createAsyncThunk('area/update', async (area:Area,api) => {
+export let update = createAsyncThunk('area/update', async (area: Area, api) => {
   let res = {} as Area
   try {
     let channel = new Channel<Area[]>(state => {
       res = state[0]
     })
     await invoke('areas_control', {
-      payload: { payload: { command: 'update',area } },
+      payload: { command: 'update', item:area },
       channel
     }).catch(err => {
       console.error('there is a problem invoking create_area: ', err)
@@ -17,25 +17,21 @@ export let update = createAsyncThunk('area/update', async (area:Area,api) => {
   } catch (err) {
     api.rejectWithValue('there is a problem retrieving an area: ' + err)
   }
-  return res as Area
+  return res
 })
 
-
-export let update_thunk_builder = (builder: ActionReducerMapBuilder<areas_storage>) => {
+export let update_thunk_builder = (
+  builder: ActionReducerMapBuilder<areas_storage>
+) => {
   builder.addAsyncThunk(update, {
     pending: state => {
       state.status = 'loading'
-      state.loading = true
     },
     fulfilled: (state, action) => {
       state.status = 'succeeded'
-      state.loading = false
-      state.list.push(action.payload)
-      state.active.area = action.payload
     },
     rejected: state => {
       state.status = 'failed'
-      state.loading = false
       state.error = 'There was an error creating the area.'
     }
   })
