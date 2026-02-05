da697ba13a07ff8fe6e98114fba53973b4d16e26
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-02-05 14:59:13 +0200

    feat: update routing to display AreasList and Databases components

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Area from "../pages/area";
 import App from "../App";
+import AreasList from "../pages/area/areas_list";
+import Databases from "../pages/Databases";
 
 
 export let router = createBrowserRouter(
     createRoutesFromElements(
         <Route element={<App />}>
-            <Route path="/" index element={<Area />} />
+            <Route path="/" index element={<AreasList />} />
+            <Route path="/databases" index element={<Databases />} />
+            <Route path="/area/:id" element={<Area />} />
         </Route>
     )
 )
