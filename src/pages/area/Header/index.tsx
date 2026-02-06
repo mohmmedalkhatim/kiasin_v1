6159e925146b4baf4a1478e3661704658b6e1177
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-02-06 17:35:15 +0200

    feat: implement AreaHeader component with editing and auto-fill functionality

---
+++ src/pages/area/Header/index.tsx
@@ -0,0 +1,56 @@
+import { IconDatabasePlus, IconEdit, IconForms, IconGrid3x3 } from "@tabler/icons-react"
+import { useSelector, useDispatch } from "react-redux"
+import { areas } from "../../../contexts/area"
+import { update } from "../../../contexts/area/functions/update"
+import { RootState, AppDispatch } from "../../../contexts/store"
+import Header from "../../../components/header"
+import HeaderAction from "./action"
+
+interface headerProps {
+    setDense: (nextValue?: any) => void
+}
+
+function AreaHeader({ setDense }: headerProps) {
+    let { area } = useSelector((state: RootState) => state.area.active)
+    let dispatch = useDispatch<AppDispatch>()
+    let name = useSelector((state: RootState) => state.area.active.area.name)
+
+    return (
+        <Header className="bg">
+            <div>{name}</div>
+            <div className="flex items-center gap-2">
+                <HeaderAction className="flex flex-col items-center" onClick={() => dispatch(areas.actions.toggle_editing())}>
+                    <IconEdit size={"1.5rem"} stroke={"1x"} />
+                    <div className="text-xs">
+                        edit
+                    </div>
+                </HeaderAction>
+                <HeaderAction className="flex flex-col items-center" onClick={async () => {
+                    setDense(!area.structure.dense)
+                    await dispatch(update({ ...area, structure: { cards: area.structure.cards, dense: !area.structure.dense } }))
+                    dispatch(areas.actions.update_active({ ...area, structure: { cards: area.structure.cards, dense: !area.structure.dense } }))
+                }}>
+                    <IconGrid3x3 size={"1.5rem"} stroke={"1x"} />
+                    <div className=" text-xs font-normal">
+                        auto fill
+                    </div>
+                </HeaderAction>
+                <HeaderAction className="flex flex-col items-center" description="add a from" onClick={() => {
+
+                }}>
+                    <IconForms size={"1.5rem"} stroke={"1x"} />
+                    <div className=" text-xs font-normal">
+                        forms
+                    </div>
+                </HeaderAction>
+                <HeaderAction className="flex flex-col items-center" description="link a database" onClick={() => { }}>
+                    <IconDatabasePlus size={"1.5rem"} stroke={"1x"} />
+                    <div className=" text-xs font-normal">
+                        database
+                    </div>
+                </HeaderAction>
+            </div>
+        </Header>
+    )
+}
+export default AreaHeader
--- src/pages/area/containers/main_gird/index.tsx
+++ src/pages/area/containers/main_gird/index.tsx
@@ -1,16 +1,20 @@
-import React, { ReactElement, ReactNode } from "react"
+import { ReactElement, ReactNode } from "react"
 import "./index.css"
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../contexts/store";
 
 
 type MainGridProps = {
     children?: ReactNode | ReactElement | ReactElement[];
+    dense: boolean;
 }
 
 
 
-function MainGrid({ children }: MainGridProps) {
+function MainGrid({ children, dense }: MainGridProps) {
+    let edit = useSelector((state: RootState) => state.area.active.edit)
     return (
-        <div className="main_grid_container">
+        <div className={(!edit ? "main_grid_container" : "editing_grid_container") + " grid_animation" + (dense ? " dense" : "")}>
             {children}
         </div>
     )
