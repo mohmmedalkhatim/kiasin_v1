d290e096e043b1155e592597ec003bc8f42cb77d
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2026-02-07 08:42:02 +0200

    feat: add CreateTableDialog component and integrate it into DatabasesList; enhance DbCard to handle click events

--- src/pages/Databases/DatabasesList/index.tsx
+++ src/pages/Databases/DatabasesList/index.tsx
@@ -1,27 +1,44 @@
-import { useEffect } from "react"
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import DbCard from "../DbCard"
-import Input from "../../../components/input"
 import Button from "../../../components/Button"
+import { Input } from "../../../components/input"
+import CreateTableDialog from "./create_table_dialog"
+import { DB } from "../../../main"
+import { useAsync } from "react-use"
 
-let list = [{name:"areas"}]
+type DatabasesListProps = {
+    setActive: Dispatch<SetStateAction<string>>
+}
 
-function DatabasesList() {
-    return (
-        <div className="flex flex-1 flex-col border-r max-h-screen pt-[4rem] min-h-screen border-[#e2e2e220]">
-            <div className="border-b border-[#e2e2e220] p-3">
-                <Input placeholder="search for data"></Input>
-            </div>
-            <div className="bg-[#e2e2e206] overflow-auto">
-                <div className="flex flex-col gap-2 p-2   m-3">
-                    {list.map(item => {
-                        return (<DbCard name={item.name} />)
-                    })}
+function DatabasesList({ setActive }: DatabasesListProps) {
+    const [open, setOpen] = useState(false)
+    const [tables, setTables] = useState<string[]>([])
+    useEffect(() => {
+        DB.select<{ name: string }[]>("select name from sqlite_master where type='table'").then((tables) => {
+            setTables(tables.map((item) => item.name))
+        })
+    }, [])
+    if (tables.length != 0) {
+        return (
+            <>
+                <div className="flex flex-1 flex-col border-r w-[16rem] z-10 max-h-[calc(100vh-2.5rem)] bg fixed min-h-[calc(100vh-2.5rem)]  pt-18  border-[#e2e2e220]">
+                    <div className="border-b border-[#e2e2e220] p-3 w-full">
+                        <Input placeholder="search for data" ></Input>
+                    </div>
+                    <div className="bg-[#e2e2e206] overflow-auto h-full grow">
+                        <div className="flex flex-col gap-2 p-2 m-3">
+                            {tables.map(item => {
+                                return (<DbCard key={item} onClick={() => setActive(item)} name={item} />)
+                            })}
+                        </div>
+                    </div>
+                    <div className="p-4 w-full flex border-t self-end border-[#e2e2e220]">
+                        <Button className="w-full" onClick={() => setOpen(true)}>create Tables</Button>
+                    </div>
                 </div>
-            </div>
-            <div className="p-4 w-full flex border-t border-[#e2e2e220]">
-                <Button className="w-full">create Tables</Button>
-            </div>
-        </div>
-    )
+                <CreateTableDialog open={open} setOpen={setOpen} />
+            </>
+        )
+    }
 }
 export default DatabasesList
--- src/pages/Databases/DbCard/index.tsx
+++ src/pages/Databases/DbCard/index.tsx
@@ -1,8 +1,8 @@
 import { IconDatabase, IconTable } from "@tabler/icons-react"
 
-function DbCard({ name }: { name: String }) {
+function DbCard({ name,onClick }: { name: String,onClick:()=>void }) {
     return (
-        <div className="gap-2 rounded-sm w-fill hover:bg-[#e2e2e220] bg-transparent flex items-center primary_button p-3">
+        <div onClick={onClick} className="gap-2 rounded-sm w-fill hover:bg-[#e2e2e220] bg-transparent flex items-center primary_button p-3">
             <IconDatabase size={"1.2rem"}/>
             <div className="text-sm">{name}</div>
         </div>
